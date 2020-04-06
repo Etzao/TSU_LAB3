398e86b32da41431b48c20ff024f5dd590a6db9b
hanetzao@gmail.com <hanetzao@gmail.com>
2020-04-07 02:05:56 +0400

    First commit

---
+++ Assets/Scripts/IHeavenBody.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IHeavenBody
+{
+    /// <summary>
+    /// Тип небесного тела.
+    /// </summary>
+    BodyType BodyType { get; }
+    /// <summary>
+    /// Передвинуть объект по оси вращения на длину пути проходимого за одни земные сутки.
+    /// </summary>
+    void RotateOneDay();
+}
---
+++ Assets/Scripts/SolarSystem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BodyType { Sun, Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune, Pluto}
+
+public class SolarSystem : MonoBehaviour
+{
+    public const float DayInSec = 0.2f;//Время в секундах означающее 1 земные сутки
+    private static float timer = 0;//Обнуляемый таймер
+
+    private static List<IHeavenBody> heavenBodies = new List<IHeavenBody>();//Список зарегистрированных планет
+
+    /// <summary>
+    /// Метод регистрирует небесный объект. С уникальным значением BodyType.
+    /// </summary>
+    /// <param name="body">Скрипт привязанный к небесному телу</param>
+    public static void RegisterBody(IHeavenBody body) {
+        for (int i = 0; i < heavenBodies.Count; i++)
+        {
+            if (heavenBodies[i].BodyType == body.BodyType) {
+                //throw new System.Exception("Данное небесное тело уже зарегистрированно.");
+                return;
+            }
+        }
+        heavenBodies.Add(body);
+    }
+
+
+    /// <summary>
+    /// Метод вызываемый каждый кадр
+    /// </summary>
+    void Update()
+    {
+        if (timer > DayInSec)
+        {
+            timer = 0;
+            PingAllBody();
+        }
+        timer += Time.deltaTime;
+    }
+
+
+    /// <summary>
+    /// Метод вращает все зарегистрированные объекты.
+    /// </summary>
+    private static void PingAllBody() {
+        foreach (var body in heavenBodies)
+        {
+            body.RotateOneDay();
+        }
+    }
+}
