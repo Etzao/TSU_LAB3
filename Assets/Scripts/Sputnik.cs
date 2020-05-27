f3eeb961fc18fab08676e4487fc08bc95ce28320
DanilSlipchenko <danil.slipchenko16@yandex.ru>
2020-05-27 19:20:50 +0400

    4

---
+++ Assets/Scripts/Sputnik.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sputnik : MonoBehaviour
+{
+    public GameObject mercury;
+    Vector3 axis;
+    Vector3 nextAxis;
+    public float angle = 1.5f;
+    float anglePassed = 0;
+    float oneCornerX = 0;
+    float oneCornerY = 0;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        axis = new Vector3(Random.Range(-1f,1f),Random.Range(-1f,1f),0); 
+        mercury = GameObject.Find("Mercury");
+         
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        anglePassed += angle;
+        axis.x -= oneCornerX * angle;
+        axis.y -= oneCornerY * angle;
+        transform.RotateAround(mercury.transform.position, axis, angle);
+
+        if (anglePassed >= 360)
+        {
+            nextAxis = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+
+            float difX = axis.x - nextAxis.x;
+            float difY = axis.y - nextAxis.y;
+
+            oneCornerX = difX / 360;
+            oneCornerY = difY / 360;
+
+            anglePassed = 0;
+        }
+    }
+}
